f56476be28b66de79b6990a105f54a07722795e6
Scott Moser <scmoser@cisco.com>
2022-08-31 11:58:22 -0700

    Make code build on Ubuntu 20.04 with gnu-efi version 3.0.9-1
    This will make code build on focal/20.04.
    The SPrint function was renamed to UnicodeSPrint later
    in (gnu-efi commit b39be0d5a91027fa58).

--- stubby_efi.h
+++ stubby_efi.h
@@ -5,5 +5,9 @@
 #else
 #include <efi.h>
 #include <efilib.h>
+// older gnu-efi do not have UnicodeSPrint (Ubuntu 20.04 version 3.0.9-1)
+#ifndef UnicodeSPrint
+#define UnicodeSPrint(fmt, ...) SPrint(fmt, ##__VA_ARGS__)
+#endif
 #endif
 #endif
